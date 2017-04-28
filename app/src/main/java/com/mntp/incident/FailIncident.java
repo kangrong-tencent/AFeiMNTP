42cc703572be84c74d09f32b7f8d8510530928e4
448468458@qq.com <hyx3215858>
2017-04-28 15:48:18 +0800

    重写UI 增加分类

--- app/src/main/java/com/mntp/incident/FailIncident.java
+++
@@ -1,8 +0,0 @@
-package com.mntp.incident;
-
-/**
- * Created by Administrator on 2015/11/4.
- */
-public interface FailIncident {
-    public  void onfail();
-}
--- app/src/main/java/com/mntp/incident/SucceedIncident.java
+++
@@ -1,8 +0,0 @@
-package com.mntp.incident;
-
-/**
- * Created by Administrator on 2015/11/4.
- */
-public interface SucceedIncident<T> {
-    public  void onSucceed(T date);
-}
